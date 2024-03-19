20a40d8bb34b87e91f7efe2b5d072d1f838ebd16
Seymen Ozdes <ozdesxseymen@gmail.com>
2024-03-19 20:52:44 +0300

    first commit

---
+++ Yummo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yummo
+//
+//  Created by Seymen Özdeş on 19.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yummo/YummoApp.swift
@@ -0,0 +1,17 @@
+//
+//  YummoApp.swift
+//  Yummo
+//
+//  Created by Seymen Özdeş on 19.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct YummoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
