21e490a15fba9d4b6a83d3278cfb9166df210400
Seymen Ozdes <ozdesxseymen@gmail.com>
2024-03-23 10:28:57 +0300

    adding recipe view

---
+++ Yummo/View/RecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeView.swift
+//  Yummo
+//
+//  Created by Seymen Özdeş on 23.03.2024.
+//
+
+import SwiftUI
+
+struct RecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeView()
+}
