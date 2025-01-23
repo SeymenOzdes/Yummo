90d3fdb318c37114594337e36005cd456b9ea792
Seymen Ozdes <ozdesxseymen@gmail.com>
2025-01-23 17:55:02 +0300

    Add recipe button has been renewed

--- Yummo/Recipe/View/RecipeHomeView.swift
+++ Yummo/Recipe/View/RecipeHomeView.swift
@@ -12,41 +12,48 @@ struct RecipeHomeView: View {
     
     var body: some View {
         NavigationStack(path: $viewModel.path) {
-            ScrollView(.horizontal) {
-                LazyHStack(spacing: 26) {
-                    ForEach(viewModel.savedEntities, id: \.self) { recipeEntity in
-                        let recipe = Recipe(entity: recipeEntity)
-                        NavigationLink(value: recipe) {
-                            CardView(recipe: recipe)
-                                .contextMenu {
-                                    Button {
-                                        viewModel.deleteRecipe(recipe: recipeEntity)
-                                    } label: {
-                                        Label("Delete", systemImage: "trash")
+            ZStack(alignment: .bottomTrailing) {
+                ScrollView(.horizontal) {
+                    LazyHStack(spacing: 26) {
+                        ForEach(viewModel.savedEntities, id: \.self) { recipeEntity in
+                            let recipe = Recipe(entity: recipeEntity)
+                            NavigationLink(value: recipe) {
+                                CardView(recipe: recipe)
+                                    .contextMenu {
+                                        Button {
+                                            viewModel.deleteRecipe(recipe: recipeEntity)
+                                        } label: {
+                                            Label("Delete", systemImage: "trash")
+                                        }
                                     }
-                                }
+                            }
                         }
                     }
+                    // lazy view yüzünden ekranın dışında daha var olmayan objeleri layout belirler ve scrollview'in doğru yere kaydırılmasını sağlar.
+                    .scrollTargetLayout()
                 }
-                // lazy view yüzünden ekranın dışında daha var olmayan objeleri layout belirler ve scrollview'in doğru yere kaydırılmasını sağlar.
-                .scrollTargetLayout()
-            }
-            .safeAreaPadding(.horizontal, 46)
-            .scrollTargetBehavior(.paging)
-            .scrollIndicators(.hidden)
-            .navigationTitle("Recipes")
-            .navigationDestination(for: Recipe.self) { recipe in
-                RecipeView(recipe: recipe)
-                    .environmentObject(viewModel)
-            }
-            .toolbar {
+                .safeAreaPadding(.horizontal, 46)
+                .scrollTargetBehavior(.paging)
+                .scrollIndicators(.hidden)
+                .navigationTitle("Recipes")
+                .navigationDestination(for: Recipe.self) { recipe in
+                    RecipeView(recipe: recipe)
+                        .environmentObject(viewModel)
+                }
+                
                 Button {
                     viewModel.showSheet.toggle()
-                } label: {
-                    Text("+")
+                }label: {
+                    Image(systemName: "plus")
+                        .frame(width: 56, height: 56) // Set the desired button size
+                        .fontWeight(.bold)
+                        .background(Color.black)
+                        .foregroundStyle(.white)
+                        .clipShape(.circle)
+                        .padding(.trailing, 20)
+                        .padding(.bottom, 16)
                 }
-                .foregroundStyle(.primary)
-                .font(.title)
+                .padding()
                 .fullScreenCover(isPresented: $viewModel.showSheet, content: {
                     AddingRecipeView()
                         .environmentObject(viewModel)
